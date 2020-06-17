0a42bbee63af465c933164fa814f354eac5b5c9f
Whexy <whexy@outlook.com>
2020-06-17 19:27:33 +0800

    Initial Commit

---
+++ SUSTechBus WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SUSTechBus WatchKit Extension
+//
+//  Created by 石文轩 on 2020/6/17.
+//  Copyright © 2020 石文轩. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
