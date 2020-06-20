1c37dd028715f839bc14d5305cddc8cbcc9764b6
Whexy <whexy@outlook.com>
2020-06-20 19:00:47 +0800

    BUG FIX
    - Fix a bug that will leads to wrong time calculation.
    - Modify UI, adding a little padding to make a more flexable view.

--- SUSTechBus WatchKit Extension/BusView.swift
+++ SUSTechBus WatchKit Extension/BusView.swift
@@ -10,61 +10,78 @@ import SwiftUI
 
 struct BusView: View {
     @State var busViewModel: BusViewModel = BusViewModel()
-
+    
     var body: some View {
         ScrollView {
             VStack {
                 VStack(alignment: .leading) {
                     HStack {
                         Image(systemName: "chevron.up")
-                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
+                            .padding(.trailing)
                         VStack(alignment: .leading) {
                             Group {
                                 Text("欣园方向")
-                                        .font(.headline)
+                                    .font(.headline)
                                 Text("上一班 \(busViewModel.XinYuanPrevious)")
                                 Text("下一班 \(busViewModel.XinYuanNext)")
-                                        .foregroundColor(Color.red)
+                                    .foregroundColor(.red)
                             }
                         }
                     }
-                    Spacer()
+                    
                     HStack {
                         Image(systemName: "chevron.down")
-                                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
+                            .padding(.trailing)
                         VStack(alignment: .leading) {
                             Group {
                                 Text("科研楼方向")
-                                        .font(.headline)
+                                    .font(.headline)
                                 Text("上一班 \(busViewModel.KeYanLouPrevious)")
                                 Text("下一班 \(busViewModel.KeYanLouNext)")
-                                        .foregroundColor(Color.red)
+                                    .foregroundColor(.red)
                             }
                         }
                     }
+                    
                 }
                 VStack {
                     Group {
                         if busViewModel.isOnWeekDay {
                             Text("今天校巴按工作日运行")
-                                    .fontWeight(.light)
+                                .fontWeight(.light)
                         } else {
                             Text("今天校巴按节假日运行")
-                                    .fontWeight(.light)
+                                .fontWeight(.light)
                         }
                     }
                     Text("当前时间 \(busViewModel.currentTime)")
-                            .fontWeight(.light)
-                            .onTapGesture {
-                                self.busViewModel = BusViewModel()
-                            }
+                        .fontWeight(.light)
                 }
-                        .font(.footnote)
-                        .foregroundColor(Color.gray)
+                .font(.footnote)
+                .foregroundColor(.secondary)
             }
         }
+        .contextMenu {
+            Button(action:refreshBus) {
+                VStack {
+                    Image(systemName: "arrow.clockwise.circle")
+                    Text("Refresh")
+                }
+            }
+        }
+        
     }
+    
+    private func refreshBus() {
+        self.busViewModel = BusViewModel()
+    }
+    
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
